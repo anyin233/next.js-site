f1af180df55ba368eb9c28d6194b4f3999e3cd5a
cydia2001 <shizukuprprpr@gmail.com>
2022-04-16 14:51:34 +0800

    Edit some style to support multi event

--- components/event-detail.js
+++ components/event-detail.js
@@ -2,11 +2,11 @@ export default function EventDetail(e){
     let event = e
     console.log(event)
     return (
-        <div className="container mt-5 p-5 flex-row border-2 rounded">
+        <div className="container flex-row">
             <ul>
             {
                 event.event.events.map((e) => {
-                    return <>
+                    return <div className="border-2 rounded p-5 mt-5">
                     <div className="font-bold text-xl">Type: {e.event_type}</div>
                     <ul className="m-3">
                         {Object.keys(e).map(fi => {
@@ -19,7 +19,7 @@ export default function EventDetail(e){
                             </>
                         })}
                     </ul>
-                    </>
+                    </div>
                 })
             }
             </ul>
